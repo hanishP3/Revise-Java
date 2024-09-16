351487590024b9aea6470abc4844b8edaba48f17
hanish <phanish3000@gmail.com>
2024-09-16 15:32:35 +0530

    add basic intro of java

---
+++ basics/Helloworld.java
@@ -0,0 +1,12 @@
+/*
+ * JAVA FOLLOWS 3 STEPS TO RUN A PROGRAM
+ *  Creating the program
+    Compiling the program
+    Running the program
+ */
+
+class HelloWorld{
+    public static void main(String args[]){
+        System.out.println("hello world");
+    }
+ }
