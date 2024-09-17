f384389f3b2f77e85d3088c337f56a510047da77
hanish <phanish3000@gmail.com>
2024-09-17 22:59:34 +0530

    add variables in java

---
+++ basics/variables.java
@@ -0,0 +1,22 @@
+  /*There are 3 types of varaibles in java
+         * 1.Local varaibles
+         * 2.instance varaibles
+         * 3.static variables
+         * 
+         * Local varaible-A varibles defined under a method or constuctor or block is known as local variable
+         * 
+         */
+
+package basics;
+public class variables {
+
+    static String name = "hanish";
+    public static void main(String[] args) {
+        //LOCAL VARAIBLE
+        int num = 89;
+        System.out.println(num);
+        //Static variable
+        System.out.println(name);
+        
+    }
+}
