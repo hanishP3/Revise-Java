811e6e80cfea4b94d253f85d58ba47d829ebad60
hanish <phanish3000@gmail.com>
2024-09-21 21:06:47 +0530

    add modify objects in java program

---
+++ basics/Modify.java
@@ -0,0 +1,33 @@
+package basics;
+
+    class Dog {
+        // Declaring and initializing the attributes
+        String breed = "cat";
+        int age = 2;
+        String color = "Black";
+      }
+      
+    public class Modify {
+        public static void main(String[] args) {
+          // Creating an object of the class Dog
+          Dog obj = new Dog();
+      
+          // Accessing class attributes & printing the values
+          System.out.println("Before modifying:");
+          System.out.println(obj.breed);
+          System.out.println(obj.age);
+          System.out.println(obj.color);
+      
+          // Modifying class attributes
+          obj.breed = "Golden Retriever";
+          obj.age = 3;
+          obj.color = "Golden";
+      
+          // Printing
+          System.out.println("\nAfter modifying:");
+          System.out.println(obj.breed);
+          System.out.println(obj.age);
+          System.out.println(obj.color);
+        }
+      }
+
