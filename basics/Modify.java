dd511588fcd0a3ed7c6acbb2862d47d33106960b
hanish <phanish3000@gmail.com>
2024-09-22 19:18:52 +0530

    add final keyword example

--- basics/Modify.java
+++ basics/Modify.java
@@ -1,17 +1,18 @@
-package basics;
 
+package basics;
     class Dog {
         // Declaring and initializing the attributes
         String breed = "cat";
         int age = 2;
         String color = "Black";
+       // final String favColor = "Grey";       If we add final kwyword before our attribute, We furthur cant modify it 
       }
       
     public class Modify {
         public static void main(String[] args) {
           // Creating an object of the class Dog
           Dog obj = new Dog();
-      
+         
           // Accessing class attributes & printing the values
           System.out.println("Before modifying:");
           System.out.println(obj.breed);
@@ -22,7 +23,8 @@ package basics;
           obj.breed = "Golden Retriever";
           obj.age = 3;
           obj.color = "Golden";
-      
+          //obj.favColor="white";
+          
           // Printing
           System.out.println("\nAfter modifying:");
           System.out.println(obj.breed);
@@ -30,4 +32,5 @@ package basics;
           System.out.println(obj.color);
         }
       }
+
 
