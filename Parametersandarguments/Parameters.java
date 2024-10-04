50bc0f31562f619cf1a61e207134570be36f88ec
hanish <phanish3000@gmail.com>
2024-10-04 20:20:28 +0530

    add parameters and arguments in java

---
+++ Parametersandarguments/Parameters.java
@@ -0,0 +1,18 @@
+/*
+*   Information can be passed to methods as a parameter. Parameters act as variables inside the method.
+    Parameters are specified after the method name, inside the parentheses. You can add as many parameters as you want, just separate them with a comma.
+
+    from below String  name , int age are parameters.
+                hanish,21 and others are called arguments.
+ */
+package parametersandarguments;
+public class Parameters {
+    static void myMethod(String name,int age){
+        System.out.println("hi " +name+ " your age is " +age);
+    }
+    public static void main(String[] args){
+        myMethod("hanish",21);
+        myMethod("leo",21);
+        myMethod("peter parker",21);
+    }
+}
