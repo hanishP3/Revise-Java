4fd212f5f9259cf7d0a3cefd55f536c120b4e551
hanish <phanish3000@gmail.com>
2024-10-08 15:45:55 +0530

    add date in java

---
+++ Dateandtime/DateTime.java
@@ -0,0 +1,10 @@
+import java.util.*;
+//the date class is present in java.util package
+//the creation of date object is 
+//Date objectNme = new Date()
+public class DateTime {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println();
+    }
+}
