2252aba717bb8e43bda60c917151e6b1f34b7b0f
hanish <phanish3000@gmail.com>
2024-10-11 22:30:21 +0530

    add constructer in java

---
+++ constructer/Main.java
@@ -0,0 +1,18 @@
+package constructer;
+
+ public class Main {
+    int num1;
+    int num2;
+  
+    public static void main(String[] args) {
+      // We didn't created any structure
+      // a default constructor will invoke here
+      Main obj_x = new Main();
+  
+      // Printing the values
+      System.out.println("num1 : " + obj_x.num1);
+      System.out.println("num2 : " + obj_x.num2);
+    }
+  }
+    
+
